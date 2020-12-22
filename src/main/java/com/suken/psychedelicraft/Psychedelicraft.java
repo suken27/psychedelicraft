e34404f4538b32817b7e8085d6da552bcbc4b719
suken27 <suken2727@gmail.com>
2020-12-22 23:48:40 +0100

    First version working with zero content.

--- src/main/java/com/suken/psychedelicraft/Psychedelicraft.java
+++ src/main/java/com/suken/psychedelicraft/Psychedelicraft.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.suken.psychedelicraft;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,16 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(Psychedelicraft.MODID)
+public class Psychedelicraft
 {
+	
+	public static final String MODID = "psychedelicraft";
+	
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public Psychedelicraft() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
